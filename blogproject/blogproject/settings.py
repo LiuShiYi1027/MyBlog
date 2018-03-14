1b8a72e8b79804e4496fc2db0887460b6249eab6
liushiyi <liushiyi@bizhehaodeMacBook-Air.local>
2018-03-14 19:44:51 +0800

    Bug Fixed

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -102,6 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
